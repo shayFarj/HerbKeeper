c28f45383921e7f2a85de181661f33ac3b0c9b51
shayFarj <shayfarjoon0@gmail.com>
2025-03-10 08:46:00 +0200

    optimize sprite texture

--- Bouncer.py
+++ Bouncer.py
@@ -5,13 +5,22 @@ import pygame.gfxdraw
 import random
 import math
 
-class Bouncer(CircleSprite.CircleSprite):
-    def __init__(self,pos):
-        super().__init__(pos,12,Constants.PASTEL_RED)
-        pygame.gfxdraw.aapolygon(self.image,[(2,8),(22,8),(12,23)],(0,0,0))
-        pygame.gfxdraw.filled_polygon(self.image,[(2,8),(22,8),(12,23)],(0,0,0))
+BOUNCER_IMAGE = pygame.Surface((Constants.BOUNCER_RADIUS*2,Constants.BOUNCER_RADIUS*2),pygame.SRCALPHA)
 
-        self.fps_clock = pygame.time.Clock()
+pygame.gfxdraw.aacircle(BOUNCER_IMAGE,Constants.BOUNCER_RADIUS,Constants.BOUNCER_RADIUS,Constants.BOUNCER_RADIUS - 1,Constants.PASTEL_RED)
+pygame.gfxdraw.filled_circle(BOUNCER_IMAGE, Constants.BOUNCER_RADIUS, Constants.BOUNCER_RADIUS, Constants.BOUNCER_RADIUS - 1,Constants.PASTEL_RED)
+
+pygame.gfxdraw.aapolygon(BOUNCER_IMAGE,[(2,8),(22,8),(12,23)],(0,0,0))
+pygame.gfxdraw.filled_polygon(BOUNCER_IMAGE,[(2,8),(22,8),(12,23)],(0,0,0))
+
+class Bouncer(pygame.sprite.Sprite):
+    def __init__(self,pos):
+        super().__init__()
+        pos2 =  (pos[0] - Constants.HERB_RADIUS, pos[1] - Constants.HERB_RADIUS)
+
+        self.rect = pygame.rect.Rect(pos2,(2*Constants.HERB_RADIUS,2*Constants.HERB_RADIUS))
+        self.radius = Constants.HERB_RADIUS
+        self.pos = pos
 
         self.dir = [random.randint(-2,2),random.randint(-1,1)]
         if self.dir[0] == 0:
@@ -20,9 +29,7 @@ class Bouncer(CircleSprite.CircleSprite):
         if self.dir[1] == 0:
             self.dir[1] = 1
     
-    def update(self,delta) -> None:
-        #delta = self.fps_clock.tick()
-    
+    def update(self,delta) -> None:    
         if(self.pos[0] > Constants.BOUNDERIES[0] or self.pos[0] < 0):
             self.dir[0] *= -1
             self.dir[1] = random.randint(-1,1)
--- Herb.py
+++ Herb.py
@@ -1,6 +1,22 @@
 import CircleSprite
 import Constants
+import pygame
 
-class Herb(CircleSprite.CircleSprite):
+
+HERB_IMAGE = pygame.Surface((Constants.HERB_RADIUS*2,Constants.HERB_RADIUS*2),pygame.SRCALPHA)
+pygame.gfxdraw.aacircle(HERB_IMAGE,Constants.HERB_RADIUS,Constants.HERB_RADIUS,Constants.HERB_RADIUS - 1,Constants.PASTEL_GREEN)
+pygame.gfxdraw.filled_circle(HERB_IMAGE, Constants.HERB_RADIUS, Constants.HERB_RADIUS, Constants.HERB_RADIUS - 1,Constants.PASTEL_GREEN)
+
+class Herb(pygame.sprite.Sprite):
     def __init__(self,pos):
-        super().__init__(pos,14,Constants.PASTEL_GREEN)
+        pos2 =  (pos[0] - Constants.HERB_RADIUS, pos[1] - Constants.HERB_RADIUS)
+
+        self.rect = pygame.rect.Rect(pos2,(2*Constants.HERB_RADIUS,2*Constants.HERB_RADIUS))
+        self.radius = Constants.HERB_RADIUS
+        self.pos = pos
+
+        self.image = HERB_IMAGE
+
+    def setPosition(self, pos):
+        self.rect.move_ip(-self.pos[0] + pos[0],-self.pos[1] + pos[1])
+        self.pos = pos
