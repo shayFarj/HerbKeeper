0ddf4a8add4ec97610633b5b653e5dd963a72d71
shayFarj <shayfarjoon0@gmail.com>
2025-05-21 00:21:31 +0300

    revamped menu and state display

--- StateBulb.py
+++ StateBulb.py
@@ -3,23 +3,43 @@ import math
 import Constants
 
 class StateBulb(CircleSprite.CircleSprite):
-    def __init__(self,maxNum, minNum,color,reverse):
+    def __init__(self,maxNum, minNum,min_color,max_color,mid_color = None):
         super().__init__((0,0),16,(10,10,10))
         
         self.maxNum = max(maxNum,minNum)
         self.minNum = min(minNum,maxNum)
-        self.color=color
-        self.reverse=reverse
+
+        self.min_color=min_color
+        self.max_color = max_color
+        self.mid_color = mid_color
+
+
     
     def setState(self,num):
-        num = max(min(num,self.maxNum),self.minNum)
-        if not self.reverse:
-            interpol= (num / (self.maxNum - self.minNum))
-        else:
-            interpol=(abs(num)/(self.maxNum -self.minNum))
 
-        if self.color == "red":
-            self.setColor((10+245*interpol,10,10))
-        
-        if self.color == "green":
-            self.setColor((10,10+245*interpol,10))
+        if self.mid_color == None:
+            c_diff = (self.max_color[0] - self.min_color[0],self.max_color[1] - self.min_color[1],self.max_color[2] - self.min_color[2])
+
+            num = min(max(num,self.minNum),self.maxNum)
+
+            interpol = (num - self.minNum) / (self.maxNum - self.minNum)
+
+            self.setColor(colorInterpol(self.min_color,self.max_color,interpol))
+        else:
+            midNum = self.minNum + (self.maxNum - self.minNum) / 2
+
+            if num > midNum:
+                num = min(num,self.maxNum)
+                interpol = (num - midNum) / (self.maxNum - midNum)
+
+                self.setColor(colorInterpol(self.mid_color,self.max_color,interpol))
+
+            else:
+                num = max(self.minNum,num)
+                interpol = (midNum - num) / (midNum - self.minNum)
+
+                self.setColor(colorInterpol(self.mid_color,self.min_color,interpol))
+            
+
+def colorInterpol(color1,color2,t):
+    return color1[0] + (color2[0] - color1[0]) * t, color1[1] + (color2[1] - color1[1]) * t, color1[2] + (color2[2] - color1[2]) * t
--- StateDisplay.py
+++ StateDisplay.py
@@ -10,19 +10,19 @@ class StateDisplay(pygame.sprite.Group):
         
         for i in range(8):
             bPos = (pos[0] + radius*math.cos((2*math.pi/8)*i), pos[1] + radius*math.sin((2*math.pi/8)*i))
-            bulb = StateBulb.StateBulb(Constants.MAX_REWARD,0,"green",False)
+            bulb = StateBulb.StateBulb(Constants.MAX_REWARD,0,(10,10,10),(0,255,0))
             bulb.setPosition(bPos)
             self.add(bulb)
 
         for i in range(8):
             bPos = (pos[0] + 96 + radius*math.cos((2*math.pi/8)*i), pos[1] + radius*math.sin((2*math.pi/8)*i))
-            bulb = StateBulb.StateBulb(0,-Constants.MAX_PUNISH,"red",True)
+            bulb = StateBulb.StateBulb(0,-Constants.MAX_PUNISH,(255,0,0),(10,10,10))
             bulb.setPosition(bPos)
             self.add(bulb)
 
         for i in range(8):
             bPos = (pos[0] +192 + radius*math.cos((2*math.pi/8)*i), pos[1] + radius*math.sin((2*math.pi/8)*i))
-            bulb = StateBulb.StateBulb(0,Constants.MAX_PUNISH/2,"red",False)
+            bulb = StateBulb.StateBulb(-Constants.MAX_PUNISH/10,Constants.MAX_PUNISH/10,(255,0,0),(0,255,0),mid_color=(10,10,10))
             bulb.setPosition(bPos)
             self.add(bulb)
 
